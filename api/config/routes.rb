8948b635e66a3d5064865bcceb8e53ee577d90a0
ganzinn <ganzinnpg@gmail.com>
2021-08-26 22:35:07 +0900

    routes設定

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      mount_devise_token_auth_for 'User', at: 'auth'
+      resources :users, only: :show do
+        resources :creators, controller: :user_creators, only: [:index, :create]
+        resources :works, controller: :user_works, only: [:index, :create]
+      end
+
+      resources :creators, only: [:show, :update, :destroy] do
+        resources :families, only: [:create, :destroy]
+      end
+
+      resources :works, only: [:show, :update, :destroy]
+    end
+  end
 end
