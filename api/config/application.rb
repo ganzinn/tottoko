b24f32bcc91dd8324981419766524c5446aa6267
ganzinn <ganzinnpg@gmail.com>
2021-08-26 23:43:53 +0900

    jbuilder null値項目非表示対応

--- api/config/application.rb
+++ api/config/application.rb
@@ -36,5 +36,7 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    Jbuilder.ignore_nil
   end
 end
