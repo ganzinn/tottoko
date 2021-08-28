9ee17e8435737957d4c3e544139dafd4e83a45b8
ganzinn <ganzinnpg@gmail.com>
2021-08-28 10:57:04 +0900

    url構造見直しによるメソッド名変更対応

--- api/app/controllers/api/v1/users_controller.rb
+++ api/app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_api_v1_user!
 
   def show
     @user = User.find(params[:id])
