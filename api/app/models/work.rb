7216b24b5e2673d98da884f35f2a2fe14585f5f7
ganzinn <ganzinnpg@gmail.com>
2021-09-03 22:42:13 +0900

    [ADD]画像データのURL取得メソッド追加

--- api/app/models/work.rb
+++ api/app/models/work.rb
@@ -1,7 +1,26 @@
 class Work < ApplicationRecord
+  include Rails.application.routes.url_helpers
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :scope
 
   belongs_to :creator
   has_many_attached :images
+
+  # 画像データのURL取得
+  def images_url
+    # binding.pry
+    if images.attached?
+      i = 0
+      count = images.length
+      imageList = []
+      while i < count
+        imageList.push(url_for(images[i]))
+        i += 1
+      end
+      return imageList
+    else
+      return nil
+    end
+  end
+
 end
