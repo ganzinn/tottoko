2eb95b6b474aa73c699a9514b0030e7095052659
ganzinn <ganzinnpg@gmail.com>
2021-09-04 20:35:58 +0900

    [FIX]アクション並び順統一

--- api/app/controllers/api/v1/user_works_controller.rb
+++ api/app/controllers/api/v1/user_works_controller.rb
@@ -2,6 +2,23 @@ class Api::V1::UserWorksController < ApplicationController
   before_action :authenticate_api_v1_user!
   before_action :user_check
 
+  def create
+    family = Family.find_by(user_id: current_api_v1_user.id, creator_id: work_params[:creator_id])
+    work = Work.new(work_params.except(:user_id))
+
+    #子ども（creator）との関係が「父母」でない場合
+    unless family && [1, 2].include?(family.relation_id)
+      render status: 401, json: {success: false, message: 'Unauthorized'} and return
+    end
+
+    if work.valid?
+      work.save!
+      render status: 201, json: {success: true }
+    else
+      render status: 422, json: {success: false, message: work.errors}
+    end
+  end
+
   def index
     user_creator_ids = Family.where(user_id: current_api_v1_user.id).pluck(:creator_id)
     select_creator_ids = user_creator_ids
@@ -24,23 +41,6 @@ class Api::V1::UserWorksController < ApplicationController
           user_work_ids << work.id if work.scope_id == 3 || work.scope.targets.include?(work.relation_id)
         end
     @user_works = Work.where(id: user_work_ids).order(date: :desc, updated_at: :desc).with_attached_images.includes(:creator)
-  end
-
-  def create
-    family = Family.find_by(user_id: current_api_v1_user.id, creator_id: work_params[:creator_id])
-    work = Work.new(work_params.except(:user_id))
-
-    #子ども（creator）との関係が「父母」でない場合
-    unless family && [1, 2].include?(family.relation_id)
-      render status: 401, json: {success: false, message: 'Unauthorized'} and return
-    end
-
-    if work.valid?
-      work.save!
-      render status: 201, json: {success: true }
-    else
-      render status: 422, json: {success: false, message: work.errors}
-    end
   end
 
   private
