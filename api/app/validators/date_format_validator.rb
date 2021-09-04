60f053a35c4fe732e4202761765d4cc78d285ed5
ganzinn <ganzinnpg@gmail.com>
2021-09-04 15:46:56 +0900

    [FIX]ActiveRecordの暗黙の型変換対応

--- api/app/validators/date_format_validator.rb
+++ api/app/validators/date_format_validator.rb
@@ -1,5 +1,6 @@
 class DateFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
+    value = record.send("#{attribute}_before_type_cast")
     begin
       Date.parse value if value.present?
     rescue ArgumentError
